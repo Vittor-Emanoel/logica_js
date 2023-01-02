b3061ba0b23e8367dce458cb8b3dbd70ae42401a
Vittor-Emanoel <vittoremanoel1234@gmail.com>
2023-01-02 13:04:40 -0300

    operador ternário

--- aula-16/if-e-else.js
+++ aula-16/if-e-else.js
@@ -1,3 +1,18 @@
-if (6 === 6) {
-  console.log('verdadeiro');
+const idade = -11
+
+if (idade > 18) {
+  console.log('voce é maior de idade')
+} else if (idade > 12) {
+  console.log('voce é menor de idade')
+} else if (idade > 4) {
+  console.log('voce é crianca')
+} else {
+  console.log('...')
 }
+
+//Operador ternário
+// condicao ? resultado_verdadeiro : resultado_falso
+
+const resultado = 6 === 6 ? 'verdadeiro' : 'falso'
+
+console.log(resultado)
